8e6c0a44f965c9876a87f6e7e91d493407c1b205
Sajawal Mushtaq <mushtaqs@roverkids.org>
2025-05-27 23:38:15 -0400

    frontend rehaul, no radzen

--- SupportTickets-Boiler/SupportTickets-Boiler/Program.cs
+++ SupportTickets-Boiler/SupportTickets-Boiler/Program.cs
@@ -1,35 +1,21 @@
-
-using Radzen;
-using SupportTickets_Boiler.Components;
-
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddRadzenCookieThemeService(options =>
-{
-    options.Name = "MyApplicationTheme"; // The name of the cookie
-    options.Duration = TimeSpan.FromDays(365); // The duration of the cookie
-});
-// Add services to the container.
-builder.Services.AddRadzenComponents();
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
 
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-app.UseAntiforgery();
+app.UseRouting();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
 
 app.Run();
